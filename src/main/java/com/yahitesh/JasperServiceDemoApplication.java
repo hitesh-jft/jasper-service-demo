fd48674a3028bbf6c819503b6b1a1b83eca88bba
yaHitesh <yahitesh.dev@gmail.com>
2017-11-19 19:47:42 +0530

    JSD#1 init

---
+++ src/main/java/com/yahitesh/JasperServiceDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yahitesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperServiceDemoApplication.class, args);
+	}
+}
